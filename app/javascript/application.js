40711bcb2d7706446b01f3981bad7c2c7d067233
Dheeraj kushwah <dheerajkushwah2015@gmail.com>
2025-04-20 13:37:27 +0530

    Added bootstrap devise view

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,4 +1,7 @@
 // Entry point for the build script in your package.json
+import "bootstrap"
+import "bootstrap/dist/css/bootstrap.min.css"
+
 import "@hotwired/turbo-rails"
 import "./controllers"
 import "./components"
